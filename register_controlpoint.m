229fdaa2c3e506cac0912ef365ad35e8f170eb60
waynewccheng <45103074+waynewccheng@users.noreply.github.com>
2018-11-17 15:19:15 -0500

    6 subplots
    Show before and after cpcorr() results.

--- register_controlpoint.m
+++ register_controlpoint.m
@@ -1,3 +1,5 @@
+% 11-17-2018
+% changed to 6 subplots
 % 11-16-2018
 % for paper revision
 % 8-15-2016
@@ -11,10 +13,14 @@ function registered = register_controlpoint (original_fn, unregistered_fn)
 original = imread(original_fn);
 unregistered = imread(unregistered_fn);
 
-%%
+% fuse two images with red and green
+imf_before = imfuse(original,unregistered,'falsecolor','Scaling','joint','ColorChannels',[1 2 0]);
+
+%% Control Points
 % use cpselect GUI to choose control points
 % and wait until control points are exported
 
+% use previous selected control points?
 if 1
     load('my_control_points','movingPoints','fixedPoints');
 else
@@ -22,49 +28,66 @@ else
     save('my_control_points','movingPoints','fixedPoints')
 end
 
-%%
+%% Registered results using control points only
+% calculate the transform
+mytform_cp = fitgeotrans(movingPoints, fixedPoints, 'NonreflectiveSimilarity');
+
+% apply the transform and clip it with the window of the original image
+registered_cp = imwarp(unregistered, mytform_cp,'OutputView',imref2d(size(original)));
+
+% fuse two images with red and green
+imf_cp = imfuse(original,registered_cp,'falsecolor','Scaling','joint','ColorChannels',[1 2 0]);
+
+
+%% Cross correlation
 % refine the control points with cross correlation
 % works only for translation (shifting on the XY plane)
 movingPointsAdjusted = cpcorr(movingPoints,fixedPoints,...
     squeeze(unregistered(:,:,2)),...
     squeeze(original(:,:,2)));
 
-%%
 % calculate the transform
-mytform = fitgeotrans(movingPointsAdjusted, fixedPoints, 'NonreflectiveSimilarity');
+mytform_cp_corr = fitgeotrans(movingPointsAdjusted, fixedPoints, 'NonreflectiveSimilarity');
 
-%%
-% apply the transform
-% and clip it with the window of the original image
-registered = imwarp(unregistered, mytform,'OutputView',imref2d(size(original)) );
+% apply the transform and clip it with the window of the original image
+registered = imwarp(unregistered, mytform_cp_corr,'OutputView',imref2d(size(original)) );
 
-%%
-% show result
-% using red and green fusion
-imf = imfuse(original,registered,'falsecolor','Scaling','joint','ColorChannels',[1 2 0]);
-save('image_fused','imf')
+% fuse two images with red and green
+imf_cp_corr = imfuse(original,registered,'falsecolor','Scaling','joint','ColorChannels',[1 2 0]);
 
-imf_before = imfuse(original,unregistered,'falsecolor','Scaling','joint','ColorChannels',[1 2 0]);
+save('image_fused','imf_cp_corr')
 
-%% visualization
-subplot(2,2,1)
+
+%% Visualization
+subplot(3,2,1)
 image(original);
 title('Truth')
+axis image
 axis off
 
-subplot(2,2,2)
+subplot(3,2,2)
 image(unregistered);
 title('WSI')
+axis image
 axis off
 
-subplot(2,2,3)
+subplot(3,2,3)
 image(imf_before);
 title('Before registration')
+axis image
 axis off
 
-subplot(2,2,4)
-image(imf);
-title('After registration')
+subplot(3,2,4)
+image(imf_cp_corr);
+title('Registration with control point only')
+axis image
 axis off
+
+subplot(3,2,5)
+image(imf_cp_corr);
+title('Registration with control point and cross correlation')
+axis image
+axis off
+
 return
 end
