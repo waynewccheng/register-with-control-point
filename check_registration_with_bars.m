d2ab306a237b7e0254614a21bfec090c618b070e
waynewccheng <45103074+waynewccheng@users.noreply.github.com>
2018-11-21 06:51:47 -0500

    Interactively choose cursor position

--- check_registration_with_bars.m
+++ check_registration_with_bars.m
@@ -1,8 +1,10 @@
-% To answer Craig's comments about registration
-function check_registration
+% To answer Craig's comments about pixel-level registration
+
+function check_registration_with_bars
 
 load('output_images','original','unregistered','registered_cp','registered_cp_corr','imf_before','imf_cp_corr')
 
+%% determine ROI
 % intionally create offset to show what if misaligned
 % ended up the original tform was not optimal
 offsetx = 0;
@@ -21,27 +23,17 @@ sizex2 = sizex2_original;
 sizey2 = sizey2_original;
 
 
-if 1
-    % see the whole image
-    sizex = 200;
-    sizey = 300;
-    
-    % use a smaller window to see the pixelation
-    sizex1 = 50;
-    sizey1 = 50;
-    
-    sizex2 = min(sizex1 + sizex - 1, sizex2_original);
-    
-    sizey2 = min(sizey1 + sizey - 1, sizey2_original);
-    
-else
-    % reduce the size by offset
-    sizex1 = sizex1 + abs(offsetx);
-    sizex2 = sizex2 - abs(offsetx);
-    
-    sizey1 = sizey1 + abs(offsety);
-    sizey2 = sizey2 - abs(offsety);
-end
+% see the whole image
+sizex = 100;
+sizey = 100;
+
+% use a smaller window to see the pixelation
+sizex1 = 50;
+sizey1 = 50;
+
+sizex2 = min(sizex1 + sizex - 1, sizex2_original);
+
+sizey2 = min(sizey1 + sizey - 1, sizey2_original);
 
 
 reg_sizex = [sizex1:sizex2]+offsetx;
@@ -62,19 +54,7 @@ imf_test = imfuse(im1,im2,...
     'falsecolor','Scaling','joint',...
     'ColorChannels',[1 2 0]);
 
-%% visualization
-
-box_start = 0.1;
-box_length = 0.8;
-
-image_ratio = 0.1;
-prof_ratio = 0.8;
-
-
-image_Position = [box_start+image_ratio*box_length box_start+image_ratio*box_length...
-    box_length*(1-image_ratio) box_length*(1-image_ratio)];
-
-
+%% determine where to profile
 % cursorx = 27;
 % cursory = 78;
 
@@ -87,8 +67,66 @@ cursory = 539;
 cursorx = 88;
 cursory = 516;
 
-cursorx = 88;
+cursorx = 44;
 cursory = 56;
+
+cursor = [44 56];
+
+while (cursor(1) >= 1 && cursor(1)<=size(im1,2) && cursor(2) >= 1 && cursor(2)<=size(im1,1))
+    
+    cursornew = compare_two_registered_images (im1, im2, cursor);
+    cursor = cursornew;
+
+end
+
+%% save data
+
+saveas(gcf,'finding registration.png')
+
+
+end
+
+
+function ret = compare_two_registered_images (im1, im2, cursor)
+
+%% annotate image by adding two lines
+
+imtmp = im1;
+imtmp(cursor(2),1:2:end,1) = 0;  % cyan
+imtmp(1:2:end,cursor(1),2) = 0;  % magenta
+
+%% get profile lines
+
+% in CIELAB
+lab1 = rgb2lab(im1);
+lab2 = rgb2lab(im2);
+
+profxline1 = lab1(cursor(2),:,1);
+profxline2 = lab2(cursor(2),:,1);
+profxline12ratio = mean(double(profxline1)./double(profxline2));
+profxline2_adjusted = profxline2 * profxline12ratio;
+profxcorrcoef = corrcoef(profxline1,profxline2_adjusted);
+
+profyline1 = lab1(:,cursor(1),1);
+profyline2 = lab2(:,cursor(1),1);
+profyline12ratio = mean(double(profyline1)./double(profyline2));
+profyline2_adjusted = profyline2 * profyline12ratio;
+profycorrcoef = corrcoef(profyline1,profyline2_adjusted);
+
+profydi = [1:size(lab1,1)];
+
+%% visualization
+
+% control parameters
+box_start = 0.1;
+box_length = 0.8;
+
+image_ratio = 0.1;
+prof_ratio = 0.8;
+
+image_Position = [box_start+image_ratio*box_length box_start+image_ratio*box_length...
+    box_length*(1-image_ratio) box_length*(1-image_ratio)];
+
 
 % new figure
 figure('Units','inches',...
@@ -104,33 +142,11 @@ set(gca,...
     'FontName','Arial');
 
 
-
-% % Truth image
-% subplot(p_total_row,p_total_column,find(p_mask_matrix_1'))
-% im1tmp(cursory,:,1) = 0;
-% im1tmp(:,cursorx,2) = 0;
-% image(im1tmp);
-% title('Truth')
-%
-% axis image
-% axis off
-
-
-% WSI image
+%% show WSI image
 graph = subplot(2,2,2);
 
 
-% in CIELAB
-lab1 = rgb2lab(im1);
-lab2 = rgb2lab(im2);
-
-im1tmp = im1;
-im2tmp = im2;
-
-im2tmp(cursory,1:2:end,1) = 0;  % cyan
-im2tmp(1:2:end,cursorx,2) = 0;  % magenta
-
-image(im2tmp);
+image(imtmp);
 axis ij
 
 set(graph,'xticklabel',[])
@@ -139,40 +155,33 @@ set(graph,'xtick',[])
 set(graph,'ytick',[])
 
 
-%% horizontal profile
+%% show horizontal profile
 
 profx = subplot(2,2,4);
 
-line1 = lab1(cursory,:,1);
-line2 = lab2(cursory,:,1);
-line12ratio = mean(double(line1)./double(line2));
-
 hold on
-plot(line1,'c-')
-plot(line2*line12ratio,'c:')
+plot(profxline1,'c-')
+plot(profxline2,'c:')
+%plot(profxline2_adjusted,'c:')
 
-xlabel(sprintf('X position (Y=%d)',cursory))
+xlabel(sprintf('X position (Y=%d), R=%.4f',cursor(2),profxcorrcoef(1,2)))
 ylabel('L^*','Rotation',0)
-axis([1 size(lab1,2) 0 100])
+%axis([1 size(lab1,2) 0 100])
 
 profx.YAxisLocation = 'right';
 
-%% vertical profile
+%% show vertical profile
 
 profy = subplot(2,2,1);
 
-line1 = lab1(:,cursorx,1);
-line2 = lab2(:,cursorx,1);
-line12ratio = mean(double(line1)./double(line2));
-di = [1:size(lab1,1)];
-
 hold on
-plot(line1,di,'m-')
-plot(line2*line12ratio,di,'m:')
+plot(profyline1,profydi,'m-')
+plot(profyline2,profydi,'m:')
+%plot(profyline2_adjusted,profydi,'m:')
 
 xlabel('L^*')
-ylabel(sprintf('Y position (X=%d)',cursorx))
-axis([0 100 1 size(lab1,1)])
+ylabel(sprintf('Y position (X=%d), R=%.4f',cursor(1),profycorrcoef(1,2)))
+%axis([0 100 1 size(lab1,1)])
 
 set(profy,'Ydir','reverse')
 
@@ -192,8 +201,6 @@ profy.Position(3) = (graph.Position(1)-box_start) * prof_ratio;
 profy.Position(4) = graph.Position(4);
 
 
-%% save data
-
-saveas(gcf,'finding registration.png')
+ret = round(ginput(1));
 
 end
